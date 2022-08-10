1b5bb11e88d97e6f02cc0aec98f13f33be7b1265
Alex <x.innv1@gmail.com>
2022-08-10 16:43:59 +0300

    go

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -4,6 +4,7 @@ import Logotype from "../Components/Logotype/Logotype";
 import StatusBar from "../Components/StatusBar/StatusBar";
 import Step from "../Components/Step/Step";
 import {Fade, Slide} from "react-reveal";
+import {NavLink} from "react-router-dom";
 
 const MainPage = () => {
     return (
@@ -16,14 +17,29 @@ const MainPage = () => {
                             <StatusBar/>
                             <Step number="1"/>
                             <h1 className={a.HelpText}>Выберите сервис <br/>
-                                <text className={a.HelpTextGreen}>Из которого</text>
+                                <text className={a.HelpTextGreen}>Из которого </text>
                                 вы хотите перенести треки
                             </h1>
                             <div className={a.Card}>
                                 <ul>
-                                    <h1>Spotife</h1>
-                                    <h1>Spotife</h1>
-                                    <h1>Spotife</h1>
+                                    <a className={a.LinkServices} href="https://spotify.com">
+                                        <li className={a.ServicesWrapper}>
+                                            <img src="../images/Services/Spotify.png" alt=""/>
+                                            <h1 className={a.ServicesTitle}>Spotify</h1>
+                                        </li>
+                                    </a>
+                                    <a className={a.LinkServices} href="https://music.yandex.ru/">
+                                        <li className={a.ServicesWrapper}>
+                                            <img src="../images/Services/Yandex.png" alt=""/>
+                                            <h1 className={a.ServicesTitle}>Yandex Music</h1>
+                                        </li>
+                                    </a>
+                                    <a className={a.LinkServices} href="https://music.apple.com/ru/">
+                                        <li className={a.ServicesWrapper}>
+                                            <img src="../images/Services/Apple.png" alt=""/>
+                                            <h1 className={a.ServicesTitle}>Apple Music</h1>
+                                        </li>
+                                    </a>
                                 </ul>
                             </div>
                         </div>
